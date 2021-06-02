62e263897ab6f4624764bcad2a1d7aa416613cf4
조규남 <gncho@mqnic.com>
2021-06-03 00:37:08 +0900

    rebuilding site 2021년 6월  3일 목요일 00시 37분 08초 KST

--- js/gistParser.js
+++ js/gistParser.js
@@ -1,3 +1,7 @@
+var scripts = document.getElementsByTagName('script');
+var myScript = scripts[ scripts.length - 1 ];
+var queryString = myScript.src.replace(/^[^\?]+\??/,'');
+
 window.onload = function () {
     var t = this;
     const userInfo = JSON.parse(document.getElementById("data").text);
